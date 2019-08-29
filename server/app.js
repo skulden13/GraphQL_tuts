9de1ffa6bb96a7761e3ab8722916d0cd4a0fce4d
Denis Skulimovskiy <skulden13@gmail.com>
2019-08-29 12:06:21 +0300

    add RootQuery & export schema

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // GraphQL Middleware
 app.use('/graphql', graphqlHTTP({
-
+  schema,
 }));
 
 app.listen(4000, () => {
