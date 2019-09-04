db74600806c9b59a1377e75ab48b6d698e4f72c0
Denis Skulimovskiy <skulden13@gmail.com>
2019-09-04 12:35:51 +0300

    add mongoose & connect MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to cloud.mongodb.com
+mongoose.connect('mongodb+srv://denis:my_super_password_123@cluster0-48jky.mongodb.net/graphql-tuts?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('Connected to database...')
+});
 
 // GraphQL Middleware
 app.use('/graphql', graphqlHTTP({
