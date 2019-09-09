8b4f04d5e3309838fc4bd9b96b56e57e3be9f440
Denis Skulimovskiy <skulden13@gmail.com>
2019-09-09 11:42:42 +0300

    upd resolve functions for schema types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,6 +21,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
+        return Author.findById(parent.authorId);
       },
     }
   }),
@@ -35,6 +36,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
+        return Book.find({authorId: parent.id});
       }
     }
   }),
@@ -47,23 +49,26 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
+        return Book.findById(args.id);
       },
     },
     author: {
       type: AuthorType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        return _.find(authors, {id: args.id});
+        return Author.findById(args.id);
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
+        return Book.find({});
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
+        return Author.find({});
       }
     }
   }),
