441a99a392e52dd7c729381575861fabac1c6bf0
Denis Skulimovskiy <skulden13@gmail.com>
2019-09-17 12:12:32 +0300

    client: add AddBook initial state, add callbcks

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,6 +4,15 @@ import { getAuthorsQuery } from '../queries/queries';
 
 
 class AddBook extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      genre: '',
+      authorId: '',
+    };
+  }
+
   displayAuthors() {
     var data = this.props.data;
 
@@ -20,23 +29,39 @@ class AddBook extends React.Component {
     }
   }
 
+  onSubmit(e) {
+    console.log(this.state);
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form id="add-book">
+        <form
+          id="add-book"
+          onSubmit={this.onSubmit.bind(this)}
+        >
           <div className="field">
             <label>Book name:</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={ (e) => this.setState({ name: e.target.value })}
+            />
           </div>
 
           <div className="field">
             <label>Genre:</label>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={ (e) => this.setState({ genre: e.target.value })}
+            />
           </div>
 
           <div className="field">
             <label>Author:</label>
-            <select>
+            <select
+              onChange={ (e) => this.setState({ authorId: e.target.value })}
+            >
               <option>Select author</option>
               {this.displayAuthors()}
             </select>
